3ba11e614ac95f536984bbcf7f93c756cd846712
Thammarong Glomjai <thammarong.g@siamintech.co.th>
2024-11-11 20:28:47 +0700

    chore: remove nextui

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { NextUIProvider } from "@nextui-org/react";
 import "./index.css";
+import "@mysten/dapp-kit/dist/index.css";
 import App from "./App.tsx";
+/* SUI dApp Components */
+import {
+  createNetworkConfig,
+  SuiClientProvider,
+  WalletProvider,
+} from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl("localnet") },
+  devnet: { url: getFullnodeUrl("devnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+      <QueryClientProvider client={queryClient}>
+        <SuiClientProvider networks={networkConfig} defaultNetwork="localnet">
+          <WalletProvider>
+            <App />
+          </WalletProvider>
+        </SuiClientProvider>
+      </QueryClientProvider>
   </StrictMode>
 );
