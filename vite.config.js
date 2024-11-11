c0dd3151af5bfde616d870c37f9bd5b83d6c542d
Sitt Guruvanich <aekazitt@gmail.com>
2024-11-11 15:47:32 +0700

    fix: add missing assets

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+/* ~~/vite.config.js */
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './pages/main.jsx',
+      output: {
+        assetFileNames: 'dist.[ext]',
+        entryFileNames: 'dist.js'
+      },
+    },
+  },
+})
