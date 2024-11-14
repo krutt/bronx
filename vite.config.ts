0a085ad8d7b11d7b60118c3dd3245f56a3a5849d
Sitt Guruvanich <aekazitt@gmail.com>
2024-11-15 02:59:05 +0700

    refactor: define postcss plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 /* ~~/vite.config.js */
 
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/bronx/',
+  css: {
+    postcss: {
+      plugins: [autoprefixer(), tailwindcss()],
+    },
+  },
   plugins: [react()],
 })
