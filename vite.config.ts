8a984f20bfd6763f1bec0e5817e45ebec01371f0
Sitt Guruvanich <aekazitt@gmail.com>
2024-11-15 02:53:44 +0700

    Revert "refactor: set static folder to publicDir"
    This reverts commit 629b219c025a7d240552f5ff11ae3e67dbb4c386.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-/* ~~/vite.config.ts */
+/* ~~/vite.config.js */
 
-import { URL, fileURLToPath } from 'node:url'
-import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/bronx/',
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
   plugins: [react()],
-  publicDir: fileURLToPath(new URL('./static', import.meta.url)),
 })
