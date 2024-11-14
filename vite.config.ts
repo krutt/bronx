629b219c025a7d240552f5ff11ae3e67dbb4c386
Sitt Guruvanich <aekazitt@gmail.com>
2024-11-15 02:51:43 +0700

    refactor: set static folder to publicDir

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-/* ~~/vite.config.js */
+/* ~~/vite.config.ts */
 
+import { URL, fileURLToPath } from 'node:url'
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/bronx/',
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [react()],
+  publicDir: fileURLToPath(new URL('./static', import.meta.url)),
 })
